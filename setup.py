957e1d782b604869fc7725a82ba2c6c52dfb0cfb
jpic <jamespic@gmail.com>
2018-09-03 03:03:19 +0200

    Release 0.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='django-call',
-    version='0.1.1',
+    version='0.1.2',
     description='Leverage uWSGI spooler and cron in Django',
     author='James Pic',
     author_email='jpic@yourlabs.org',
